bf6837bda9066f1bc8100c5276aa25b02a171a33
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-04-25 15:19:09 -0400

    create simple HTTP service

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// path: /echo/:word
+// renders the word input in the URL path in an H1 element
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	e := r.URL.Path[len("/echo/"):]
+	fmt.Fprintf(w, "<h1>%s</h1>", e)
+}
+
+// handles API request routing
+func handleRequests() {
+	http.HandleFunc("/echo/", echoHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+// entrypoint to application
+func main() {
+	handleRequests()
+}

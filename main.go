7f6594efb5a7d1c53adc5794b77a48970ef33afc
Raquel Roman-Rodriguez <rromanrodriguez@paperlesspost.com>
2022-05-06 11:35:22 -0400

    create processor package

--- main.go
+++ main.go
@@ -1,82 +1,12 @@
 package main
 
 import (
+	"example/getgoing/processor"
 	"fmt"
 	"log"
 	"net/http"
 	"strings"
 )
-
-type Reverser interface {
-	Reverse() string
-}
-
-type Echoer interface {
-	Echo() string
-}
-
-type VowelRemover interface {
-	VowelRemove() string
-}
-
-type Mirrorer interface {
-	Mirror() string
-}
-
-type MyString string
-
-// reverses a MyString
-func (s MyString) Reverse() (result string) {
-	for _, c := range s {
-		result = string(c) + result
-	}
-	return
-}
-
-// echoes a MyString
-func (s MyString) Echo() string { return string(s) }
-
-var vowels = map[string]bool{
-	"a": true,
-	"e": true,
-	"i": true,
-	"o": true,
-	"u": true,
-}
-
-// removes vowels from a MyString
-func (s MyString) VowelRemove() (result string) {
-	for _, c := range s {
-		if vowels[string(c)] {
-			continue
-		} else {
-			result = string(c) + result
-		}
-	}
-	return
-}
-
-// mirrors a MyString
-func (s MyString) Mirror() (result string) {
-	result = string(s)
-	for _, c := range s {
-		result = string(c) + result
-	}
-	return
-}
-
-type StringManipulator struct {
-	s            MyString
-	manipulation map[string]interface{}
-}
-
-func (t *StringManipulator) manipulate() {
-	t.manipulation = make(map[string]interface{})
-	t.manipulation["echo"] = t.s.Echo()
-	t.manipulation["reverse"] = t.s.Reverse()
-	t.manipulation["mirror"] = t.s.Mirror()
-	t.manipulation["vowelremove"] = t.s.VowelRemove()
-}
 
 // handles API request routing
 func handleRequests() {
@@ -84,27 +14,24 @@ func handleRequests() {
 		// select the path
 		p := r.URL.Path[1:]
 		i := strings.Index(p, "/")
-		sm := new(StringManipulator)
 
 		// if there is a '/' in `p`, we will see if it is followed by a key in sm.manipulate
 		if i > 0 {
-			sm.s = MyString(p[:i])
-			sm.manipulate()
+			sm := processor.NewMyString(p[:i])
 			fn := strings.ToLower(p[i+1:])
-			_, exists := sm.manipulation[fn]
+			_, exists := sm.Manipulators[fn]
 			// if yes, we will render it
 			if exists {
-				fmt.Fprintf(w, "<h1>%s: %s</h1>", fn, sm.manipulation[fn])
+				fmt.Fprintf(w, "<h1>%s: %s</h1>", fn, sm.UseManipulation(fn))
 			} else {
 				// otherwise we will render the word with a suggestion
-				fmt.Fprintf(w, "<h1>%s</h1><h3>pst, try replacing '%s' with one of the manipulators:</h3><ul><li>echo</li><li>mirror</li><li>reverse</li><li>vowelremove</li></ul>", sm.s, fn)
+				fmt.Fprintf(w, "<h1>%s</h1><h3>pst, try replacing '%s' with one of the manipulators:</h3><ul><li>echo</li><li>mirror</li><li>reverse</li><li>vowelremove</li></ul>", sm.S, fn)
 			}
 			// if there is no "/" in `p`, we show all of the manipulators
 		} else {
-			sm.s = MyString(p)
-			sm.manipulate()
-			for key, val := range sm.manipulation {
-				fmt.Fprintf(w, "<h1>%s: %s</h1>", key, val)
+			sm := processor.NewMyString(p)
+			for key := range sm.Manipulators {
+				fmt.Fprintf(w, "<h1>%s: %s</h1>", key, sm.UseManipulation(key))
 			}
 		}
 
